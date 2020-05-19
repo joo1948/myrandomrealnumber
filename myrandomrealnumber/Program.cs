1b276caf47de717f1a695557a9075ce77536d477
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-19 10:10:42 +0900

    문제 5번 myrandomrealnumber

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,6 +10,26 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int num = random.Next() % 1000;
+            Console.WriteLine("컴퓨터가 생각한 숫자 : " + num);
+            
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요.: ");
+                int mynum = int.Parse(Console.ReadLine());
+                
+                if (num == mynum)
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
+                
+
+                if(mynum < num) { Console.WriteLine(mynum + "보다는 큰 숫자입니다."); }
+                else if (mynum > num) { Console.WriteLine(mynum + "보다는 작은 숫자입니다." ); }
+                else { Console.WriteLine("잘못입력하셨습니다."); }
+            }
         }
     }
 }
